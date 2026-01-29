4710b39ae2fddb7c97d132effe8ea4950cd27cb1
ghost <ghost@ghosts-Mac-mini.local>
2026-01-28 20:42:42 -0600

    adding custom components

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,48 @@
+import Button from "./components/ButtonSite"
+import Card, { type CardProps } from "./components/Card"
+import Chip from "./components/Chip"
+import type { ProjectCardProps } from "./components/ProjectCard"
+import ProjectCard from "./components/ProjectCard"
+
 function App() {
+
+
+
+  const metrics: CardProps[] = [
+    {
+      labelCard: "YEARS",
+      description: "5+",
+      variant: "main"
+    }, {
+      labelCard: "Clients",
+      description: "5+",
+    }, {
+      labelCard: "Projects",
+      description: "5+",
+    }, {
+      labelCard: "Tools",
+      description: "5+",
+    }
+  ]
+
+
+  const techStack = ["React", "TS", "Node", "SQL"]
+
+  const projects: ProjectCardProps[] = [
+   {
+      img: "img",
+      
+      type: "fullstack",
+      title: "project title",
+      description: "descriptions here"
+   }, {
+      img: "img",
+      type: "fullstack",
+      title: "project title",
+      description: "descriptions here"
+   }
+  ]
+
   return (
     <div className='layout'>
       <nav className="navigation p-6">
@@ -11,27 +55,14 @@ function App() {
           <h1 className="text-white font-display">FULLSTACK DEVELOPER</h1>
           <p className="text-gray-600 text-xl font-mono">Building scalable web applications with modern technologies</p>
           <div className="btn-container">
-            <button type="button" className="button-site bg-dark-surface text-white text-base font-semibold ">Download_cv()</button>
-            <button type="button" className="button-site bg-orange text-base font-semibold ">View_github()</button>
+            <Button variant="dark">Download_cv()</Button>
+            <Button>View_github()</Button>
           </div>
         </section>
         <section className="metrics-container">
-          <div className="metric-card bg-orange p5">
-            <span className="font-mono text-base">YEARS</span>
-            <h6 className="font-display text-5xl ">5+</h6>
-          </div>
-          <div className="metric-card bg-card p5">
-            <span className="font-mono text-gray-600 text-base">Clients</span>
-            <h6 className="font-display text-white text-5xl ">5+</h6>
-          </div>
-          <div className="metric-card bg-card p5">
-            <span className="font-mono text-gray-600 text-base">Projects</span>
-            <h6 className="font-display text-white text-5xl ">5+</h6>
-          </div>
-          <div className="metric-card bg-card p5">
-            <span className="font-mono text-gray-600 text-base">Tools</span>
-            <h6 className="font-display text-white text-5xl ">5+</h6>
-          </div>
+          {metrics.map(metric => (
+            <Card {...metric} />
+          ))}
         </section>
         <section className="projects-container">
           <span className="font-mono text-orange">// PROJECTS</span>
@@ -40,32 +71,14 @@ function App() {
             <a className="font-mono text-orange" href="http://">Show_all()</a>
           </div>
           <div className="card-container gap-5">
-            <div className="card">
-              img
-              <div className="description bg-card gap-4 p-6">
-                <span className="font-mono text-orange text-base">type: fullstack</span>
-                <span className="font-mono text-white text-xl">project title</span>
-                <p className="font-mono text-gray-600 text-xl">descriptions here</p>
-              </div>
-            </div>
-            <div className="card">
-              img
-              <div className="description bg-card gap-4 p-6">
-                <span className="font-mono text-orange text-base">type: fullstack</span>
-                <span className="font-mono text-white text-xl">project title</span>
-                <p className="font-mono text-gray-600 text-xl">descriptions here</p>
-              </div>
-            </div>
+            {projects.map(project => <ProjectCard {...project} />)}
           </div>
         </section>
         <section className="stack-container gap-5">
           <span className="font-mono text-orange">// TECH_STACK</span>
           <h3 className="font-display text-white text-2xl">TECHNOLOGIES</h3>
           <div className="chip-container gap-4">
-            <div className="bg-dark-surface font-mono text-md text-white p-4 border-placeholder">React</div>
-            <span className="bg-dark-surface font-mono text-md text-white p-4 border-placeholder">TS</span>
-            <span className="bg-dark-surface font-mono text-md text-white p-4 border-placeholder">Node</span>
-            <span className="bg-dark-surface font-mono text-md text-white p-4 border-placeholder">SQL</span>
+            {techStack.map(tech => <Chip label={tech} />)}
           </div>
         </section>
         <section className="contact bg-card p-8 gap-5">
@@ -73,7 +86,7 @@ function App() {
           <h3 className="font-display text-white text-2xl">LET'S BUILD</h3>
           <p className="font-mono text-gray-600 text-xl">Available for freelance and full-time opportunities</p>
           <span className="text-teal font-mono text-base">dev.tristan.romero@gmail.com</span>
-          <button type="button" className="button-site bg-orange text-base font-semibold ">Contact()</button>
+          <Button>Contact()</Button>
         </section>
       </section>
     </div>
