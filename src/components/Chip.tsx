ca1b275311aba8d46ca145b0f0dc3100060f8022
Ismael-devTR <dev.tristan.romero@gmail.com>
2026-03-02 15:32:17 -0600

    adding image support

--- src/components/Chip.tsx
+++ src/components/Chip.tsx
@@ -1,13 +1,24 @@
-import  { type FC } from 'react'
+import { type FC } from "react";
 
 interface ChipProps {
-    label: string
+  label: string;
+  iconUrl?: string;
 }
 
-const Chip: FC<ChipProps> = ({ label }: ChipProps) => {
-    return (
-        <div className="bg-dark-surface font-mono text-md text-white p-4 border-placeholder">{label}</div>
-    )
-}
+const Chip: FC<ChipProps> = ({ label, iconUrl }: ChipProps) => {
+  return (
+    <div className="bg-dark-surface font-mono text-md text-white p-4 border-placeholder chip flex flex-row items-center">
+      {iconUrl && (
+        <img
+          src={iconUrl}
+          alt={`${label} icon`}
+          className="mr-2 shrink-0 object-contain"
+          style={{ width: "40px", height: "40px" }}
+        />
+      )}
+      {label}
+    </div>
+  );
+};
 
-export default Chip
+export default Chip;
